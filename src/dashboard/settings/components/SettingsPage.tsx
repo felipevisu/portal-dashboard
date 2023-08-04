aa5f6a1b1db9fbd9cb54d7669e732b1b4436c198
Felipe Faria <felipevisu@gmail.com>
2023-08-03 22:55:57 -0300

    refactor: rename document queryes

--- src/dashboard/settings/components/SettingsPage.tsx
+++ src/dashboard/settings/components/SettingsPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { Box, Typography } from "@mui/material";
 
 import { SettingsItem } from "../utils";
 
-import { NavigationCard, SettingsGrid, SettingsLink } from "./styles";
+import { NavigationCard, SettingsGrid } from "./styles";
 
 interface SettingsPageProps {
   menus: SettingsItem[];
@@ -14,7 +15,7 @@ export const SettingsPage = ({ menus }: SettingsPageProps) => {
   return (
     <SettingsGrid>
       {menus.map((menu) => (
-        <SettingsLink to={menu.url} key={menu.url}>
+        <Link to={menu.url} key={menu.url}>
           <NavigationCard>
             <Typography>{menu.icon}</Typography>
             <Box>
@@ -24,7 +25,7 @@ export const SettingsPage = ({ menus }: SettingsPageProps) => {
               <Typography>{menu.description}</Typography>
             </Box>
           </NavigationCard>
-        </SettingsLink>
+        </Link>
       ))}
     </SettingsGrid>
   );
--- src/dashboard/settings/components/styles.tsx
+++ src/dashboard/settings/components/styles.tsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
-
 import { Paper, styled } from "@mui/material";
 
 export const SettingsGrid = styled("div")(({ theme }) => ({
   display: "grid",
-  gap: theme.spacing(4),
-  gridTemplateColumns: "1fr 1fr 1fr",
-}));
-
-export const SettingsLink = styled(Link)(() => ({
-  borderRadius: 8,
+  gap: theme.spacing(3),
+  gridTemplateColumns: "1fr",
+  [theme.breakpoints.up("md")]: {
+    gridTemplateColumns: "1fr 1fr",
+  },
+  [theme.breakpoints.up("xl")]: {
+    gridTemplateColumns: "1fr 1fr 1fr",
+  },
 }));
 
 export const NavigationCard = styled(Paper)(({ theme }) => ({
@@ -17,4 +17,5 @@ export const NavigationCard = styled(Paper)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   gap: theme.spacing(3),
+  height: "100%",
 }));
